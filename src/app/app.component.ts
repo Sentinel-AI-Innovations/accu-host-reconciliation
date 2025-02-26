aa0a9876387c64d5cc0b99b5b765e4a50a116bea
Thomas Martin <ciscofu@gmail.com>
2025-02-25 22:22:28 -0500

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'accu-host-reconciliation';
+  host_equiv = {
+    "srv028": "wycom"
+  }
+
+  ignore_host_prefixes = {
+    "intune": ["accu-", "srv", "sv"],
+  }
+
+  fileChange(target: EventTarget | null) {
+    const targetElement = target as HTMLInputElement;
+    const file = targetElement.files?.item(0);
+    if (file) {
+      let excelData: any;
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const arrayBuffer: any = fileReader.result;
+        const data = new Uint8Array(arrayBuffer);
+        const workbook = XLSX.read(data, {type: 'array'});
+        excelData = XLSX.utils.sheet_to_json(workbook.Sheets["Kace"]);
+        console.log(excelData);
+      };
+      fileReader.readAsArrayBuffer(file);
+    }
+  }
 }
