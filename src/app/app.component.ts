4cd38f49c0d70cf34eaaf8563e37d5f6fcf21dcf
Thomas Martin <ciscofu@gmail.com>
2025-02-26 20:03:49 -0500

    Updated to avoid fs dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import {Component} from '@angular/core';
-import * as XLSX from 'xlsx';
-import xlsx from 'node-xlsx';
 import {MatIcon} from '@angular/material/icon';
 import {DatePipe} from '@angular/common';
+import * as Excel from 'exceljs';
 
 @Component({
   selector: 'app-root',
@@ -28,6 +27,7 @@ export class AppComponent {
   tenable: any;
   intune: any;
   blumira: any;
+  workbook!: Excel.Workbook;
 
   warnings: string[] = [];
   results: { [key: string]: string[] } = {};
@@ -38,16 +38,20 @@ export class AppComponent {
     const file = targetElement.files?.item(0);
     if (file) {
       const fileReader = new FileReader();
-      fileReader.onload = (e) => {
+      fileReader.onload = async (e) => {
         this.warnings = [];
         this.results = {};
 
-        this.kace = this.readExcelTab(fileReader, "Kace");
-        this.ad = this.readExcelTab(fileReader, "AD");
-        this.trend = this.readExcelTab(fileReader, "Trend");
-        this.tenable = this.readExcelTab(fileReader, "Tenable");
-        this.intune = this.readExcelTab(fileReader, "Intune");
-        this.blumira = this.readExcelTab(fileReader, "Blumira");
+        const arrayBuffer: any = fileReader.result;
+        const workbook = new Excel.Workbook();
+        this.workbook = await workbook.xlsx.load(arrayBuffer);
+
+        this.kace = this.readExcelTab("Kace");
+        this.ad = this.readExcelTab("AD");
+        this.trend = this.readExcelTab("Trend");
+        this.tenable = this.readExcelTab("Tenable");
+        this.intune = this.readExcelTab("Intune");
+        this.blumira = this.readExcelTab("Blumira");
 
         if (this.kace.length == 0) {
           this.warnings.push("No Kace data found");
@@ -78,33 +82,36 @@ export class AppComponent {
     }
   }
 
-  find_record(data: any, nameCol: string, name: string) {
+  find_record(data: any, nameHeader: string, name: string) {
     // Strip off the domain from the hostname, if there is a domain
-    name = name.trim().toLowerCase();
-    let idx = name.indexOf(".");
-    if (idx != -1)
-      name = name.substring(0, idx);
+    name = this.host(name.trim().toLowerCase());
 
     // Substitute names, if appropriate
     if (this.host_equiv[name])
       name = this.host_equiv[name];
 
-    for (const row of data) {
-      let actualName = row[nameCol].trim().toLowerCase();
-      let idx = name.indexOf(".");
-      if (idx != -1)
-        actualName = actualName.substring(0, idx);
+    let nameColIdx = this.get_column_index(nameHeader, data);
+    for (let i = 1; i < data.length; ++i) {
+      const row = data[i];
+      let actualName = this.host(row[nameColIdx].trim().toLowerCase());
       if (this.host_equiv[actualName])
         actualName = this.host_equiv[actualName];
 
       if (actualName == name)
         return row;
     }
-
     return undefined;
   }
 
-  evaluate_by_name(name: string, data: any, nameCol: string, ipCol: string = "", skip_prefixes: string[] = []) {
+  get_column_index(name: string, data: any[]): number {
+    let row = data[0];
+    if (Array.isArray(row)) {
+      row = data[0];
+    }
+    return row.indexOf(name);
+  }
+
+  evaluate_by_name(name: string, data: any, nameHeader: string, ipHeader: string = "", skip_prefixes: string[] = []) {
     let evalResults = this.results[name];
     if (!evalResults) {
       evalResults = [];
@@ -112,10 +119,13 @@ export class AppComponent {
     }
 
     // Look at each row in Kace
-    for (const k_row of this.kace) {
-      if (!k_row["Name"])
-        continue;
-      const kaceName = k_row["Name"].toLowerCase().trim();
+    const kaceNameIdx = this.get_column_index("Name", this.kace);
+    const kaceIpIdx = this.get_column_index("IP Address", this.kace);
+    const dataNameIdx = this.get_column_index(nameHeader, data);
+    const dataIpIdx = this.get_column_index(ipHeader, data);
+    for (let i = 1; i < this.kace.length; ++i) {
+      const k_row = this.kace[i];
+      const kaceName = k_row[kaceNameIdx].toLowerCase().trim();
 
       let skip = false;
       for (const prefix of skip_prefixes) {
@@ -127,12 +137,10 @@ export class AppComponent {
       if (skip)
         continue;
 
-      const rec = this.find_record(data, nameCol, kaceName);
+      const rec = this.find_record(data, nameHeader, kaceName);
       if (rec) {
-        if (ipCol && rec[ipCol]) {
-          if (k_row["IP Address"] != rec[ipCol] && !rec[nameCol].toLowerCase().includes("lap")) {
-            evalResults.push(`${kaceName} exists but has the wrong IP address`);
-          }
+        if (ipHeader && kaceIpIdx != -1 && rec[dataIpIdx] && k_row[kaceIpIdx] != rec[dataIpIdx] && !rec[dataNameIdx].toLowerCase().includes("lap")) {
+          evalResults.push(`${kaceName} exists but has the wrong IP address`);
         }
       } else {
         evalResults.push(`${kaceName} exists in Kace but not ${name}`);
@@ -140,8 +148,9 @@ export class AppComponent {
     }
 
     // Look at each row in the target record set
-    for (const d_row of data) {
-      const hostname = d_row[nameCol];
+    for (let idx = 1; idx < data.length; ++idx) {
+      const d_row = data[idx];
+      const hostname = d_row[dataNameIdx].toLowerCase().trim();
       const rec = this.find_record(this.kace, "Name", hostname);
       if (!rec) {
         evalResults.push(`${hostname} exists in ${name} but not Kace`);
@@ -157,11 +166,15 @@ export class AppComponent {
     this.evaluate_by_name("Intune", this.intune, "Device name", "", this.ignore_host_prefixes["intune"])
   }
 
-  readExcelTab(fr: FileReader, sheet: string) {
-    const arrayBuffer: any = fr.result;
-    const data = new Uint8Array(arrayBuffer);
-    const workbook = XLSX.read(data, {type: 'array'});
-    return XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+  readExcelTab(sheet: string): any[] {
+    const worksheet = this.workbook.getWorksheet(sheet)!;
+    const ret: any[] = [];
+    worksheet.eachRow((row, rowNumber) => {
+      const r = row.values as any[];
+      r.shift(); // First cell is always empty
+      ret.push(r);
+    });
+    return ret;
   }
 
   goBack() {
@@ -171,7 +184,7 @@ export class AppComponent {
   protected readonly JSON = JSON;
   protected readonly Object = Object;
 
-  downloadExcel() {
+  async downloadExcel() {
     const data = [];
 
     const date = new Date();
@@ -185,7 +198,11 @@ export class AppComponent {
         data.push([key, issue]);
       }
     }
-    const buffer = xlsx.build([{name: 'Asset Analysis Results', data: data, options: {}}]);
+
+    const workbook = new Excel.Workbook();
+    const sheet = workbook.addWorksheet("Asset Analysis Results");
+    sheet.addRows(data);
+    const buffer = await workbook.xlsx.writeBuffer();
     const blob = new Blob([buffer], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
@@ -193,6 +210,13 @@ export class AppComponent {
     a.download = 'asset_analysis_results.xlsx';
     a.click();
     window.URL.revokeObjectURL(url);
+  }
+
+  host(fqdn: string): string {
+    const idx = fqdn.indexOf(".");
+    if (idx == -1)
+      return fqdn;
+    return fqdn.substring(0, idx);
   }
 
   protected readonly Date = Date;
